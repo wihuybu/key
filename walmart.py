236d34d6e9f78b7b020dae4eb7a2dafa63df47e3
Huy Bu <70420398+wihuybu@users.noreply.github.com>
2024-03-31 23:43:18 +0700

    Update walmart.py

--- walmart.py
+++ walmart.py
@@ -82,4 +82,5 @@ def tracking():
 		return jsonify({'msg': 'Something went wrong!', 'ok': False})
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080, debug=True)
